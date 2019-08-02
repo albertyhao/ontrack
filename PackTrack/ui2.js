1b3b8c30d45cb4a2d52d298207b78761c527b7be
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2019-08-02 10:40:19 -0700

    semi-finalized cos sim

--- PackTrack/ui2.js
+++ PackTrack/ui2.js
@@ -1,9 +1,9 @@
-
 var siteText;
-
 var textBookText;
+
 var url;
 var $console = document.getElementById('console');
+var $progress = document.getElementById('progress');
 var stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 // var stemmer = require('stemmer');
 
@@ -19,13 +19,26 @@ function getWordsFromFile(fileToLoad) {
 
 
   xhr.addEventListener("load", function() {
+    $progress.innerHTML = 'Loaded Text Book';
     var result = xhr.responseText;
    // "result" should be the textbook entry for the subject
     // document.querySelector('#textBook').innerHTML = result;
    
+
+
     textBookText = result;
     if(textBookText && siteText) {
-      getSim(textBookText,siteText);
+      document.getElementById('siteText').innerHTML = siteText;
+      // siteText = document.getElementById('siteText').textContent;
+      document.getElementById('textBook').innerHTML = textBookText;
+      // console.log('hello');
+      // document.getElementById('textBook').innerHTML = textBookText;
+      
+      document.getElementById('similarity').innerHTML = getSim(siteText, textBookText);
+      // document.getElementById('similarity').innerHTML = text.substr(0,100) + '<hr>' + siteText.substr(0, 100) + '<hr>'; //siteText.length - text.length;
+
+      //document.getElementById('similarity').innerHTML = getSim(text,text2);
+      
     }
 
   });
@@ -35,8 +48,9 @@ function getWordsFromFile(fileToLoad) {
 
 
 function scrapeUserSite() {
+  $progress.innerHTML = 'Scraping Page';
   var tags = Array.from($console.querySelectorAll('*'));
-  var f = tags.filter(t => !['script', 'meta', 'link', 'input', 'html', 'body', 'head', 'style'].includes(t.tagName.toLowerCase()));
+  var f = tags.filter(t => !['script', 'meta', 'link', 'input', 'html', 'body', 'head', 'style', 'img', 'iframe'].includes(t.tagName.toLowerCase()));
   var t = f.map(
       e => Array.from(e.childNodes)
        .filter(c => c.nodeName === "#text")
@@ -47,12 +61,13 @@ function scrapeUserSite() {
       .filter(a => a && a.length > 10)
     )
     .filter(q => q.length);
-  siteText = t.join();
+  siteText = t.join(' ');
+  $progress.innerHTML = 'Done Scraping Page';
 
 }
 
 function setSubject(fileToLoad){
-   
+  $progress.innerHTML = 'Loading Text Book';
     var fileToLoad;
     var newSubject = document.querySelector("select").value;
     
@@ -70,13 +85,7 @@ function setSubject(fileToLoad){
       fileToLoad = "test.txt";
     }
    
-
-
-    
-    
-    // document.getElementById('siteText').innerHTML = siteText;
-    // document.getElementById('textBook').innerHTML = textBookText;
-    document.getElementById('similarity').innerHTML = getSim(siteText, textBookText);
+    // document.getElementById('similarity').innerHTML = getSim(siteText, textBookText);
 
     getWordsFromFile(fileToLoad);
 
@@ -116,8 +125,8 @@ function union_arrays (x, y) {
 }
 
 function getSim(str1, str2){
-  str1 = str1.split(" ").filter(w => !stopWords.includes(w)).join(" ")
-  str2 = str2.split(" ").filter(w => !stopWords.includes(w)).join(" ")
+  str1 = str1.split(" ").filter(w => !stopWords.includes(w)).join(" ");
+  str2 = str2.split(" ").filter(w => !stopWords.includes(w)).join(" ");
 
 
   var dict1 = genFreq(str1)
@@ -164,6 +173,7 @@ function  scrapePage(tabs){
 
   req.open('GET', url, true);
   req.onreadystatechange = function (){
+    $progress.innerHTML = 'Recieved Page';
     var pageContent = req.response;
     $console.style.display = 'none';
     $console.innerHTML = pageContent;
@@ -172,6 +182,7 @@ function  scrapePage(tabs){
   req.send();
 }
  
+$progress.innerHTML = 'Starting...';
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, scrapePage);
 document.querySelector("button").addEventListener("click", setSubject); // select button on ui2 html
 
