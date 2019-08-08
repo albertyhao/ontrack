b53a52391fe9ca928e3b6523664c4564d6abb540
Alina Chen <ajcheng7@gmail.com>
2019-08-07 22:00:21 -0700

    finished sending url

--- PackTrack/contentscript.js
+++ PackTrack/contentscript.js
@@ -5,6 +5,11 @@ chrome.runtime.sendMessage("agdppcjhkldbkgiopgcokdkfokjnfogj", {greeting: "hello
 
 console.log('p')
 
+chrome.storage.sync.get(['customerid'], function(){
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result}&site=${location.href}`, true);
+      xhr.send(); 
+    })
 
 // // var s = document.createElement('script');
 // // s.src = chrome.extension.getURL('blacklist.js');
