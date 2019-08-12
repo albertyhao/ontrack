c3d454f80cf36f1ae88b958d4134f9e80dc16206
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2019-08-12 16:31:05 -0700

    did stuff as well

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -1,15 +1,27 @@
 // chrome.webNavigation.onCompleted.addListener(function() {
 //      alert("I love hulu!");
 //  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
-// use for the blacklist
+// //use for the blacklist
 
- chrome.webNavigation.onCompleted.addListener(function() {
-   var s = document.createElement('script');
-   // TODO: add "script.js" to web_accessible_resources in manifest.json
-   s.src = chrome.extension.getURL('injectBackground.js');
-   s.onload = function() {
-       this.remove();
-   };
-   (document.head || document.documentElement).appendChild(s);
+//  chrome.webNavigation.onCompleted.addListener(function() {
+//    var s = document.createElement('script');
+//    // TODO: add "script.js" to web_accessible_resources in manifest.json
+//    s.src = chrome.extension.getURL('injectBackground.js');
+//    s.onload = function() {
+//        this.remove();
+//    };
+//    (document.head || document.documentElement).appendChild(s);
 
-  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+//   }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+
+
+var similarity = chrome.extension.getViews({
+    type: "popup"
+});
+for (var i = 0; i < similarity.length; i++) {
+    similarity[i].document.getElementById('similarity').innerHTML = "My Custom Value";
+}
+
+
+  
+
