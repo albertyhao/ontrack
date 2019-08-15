2aec97a5c122d047a5952d9460b3a000c799ba47
Alina Chen <ajcheng7@gmail.com>
2019-08-14 20:19:18 -0700

    moved url extraction to master branch

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -1,15 +1,22 @@
 // chrome.webNavigation.onCompleted.addListener(function() {
 //      alert("I love hulu!");
-//  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
-// use for the blacklist
+// //  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+// // use for the blacklist
 
- chrome.webNavigation.onCompleted.addListener(function() {
-   var s = document.createElement('script');
-   // TODO: add "script.js" to web_accessible_resources in manifest.json
-   s.src = chrome.extension.getURL('injectBackground.js');
-   s.onload = function() {
-       this.remove();
-   };
-   (document.head || document.documentElement).appendChild(s);
+//  chrome.webNavigation.onCompleted.addListener(function() {
+//    var s = document.createElement('script');
+//    // TODO: add "script.js" to web_accessible_resources in manifest.json
+//    s.src = chrome.extension.getURL('injectBackground.js');
+//    s.onload = function() {
+//        this.remove();
+//    };
+//    (document.head || document.documentElement).appendChild(s);
 
-  }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+//   }, {url: [{urlMatches : 'https://www.hulu.com'}]});
+
+
+chrome.storage.sync.get(['customerid'], function(result){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result}&site=hello`, true);
+  xhr.send(); 
+})
