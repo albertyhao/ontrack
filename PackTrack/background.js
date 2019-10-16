f5d5f4f9deee368129c2eca5e275cc76a22f202c
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2019-10-16 09:17:44 -0700

    background finalized

--- PackTrack/background.js
+++ PackTrack/background.js
@@ -1,8 +1,4 @@
-// chrome.storage.sync.get(['customerid'], function(){
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result}&site=${location.href}`, true);
-//   xhr.send();
-// })
+
 var stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 var subjectWords;
 var newSubject;
@@ -23,33 +19,7 @@ var linearWords = ['matrix', 'matrices', 'row', 'operation', 'column','invertibl
 var calcWords = ['area', 'under', 'calculus', 'curve', 'derivative', 'differential', 'displaystyle', 'dx', 'function', 'infinitesimal', 'infinitesimals', 'input', 'integral', 'leibniz', 'limit', 'newton', 'slope', 'squaring', 'equation', 'equations','limit', 'graph', 'theorem', 'substitution', 'interval', 'continuity', 'continous', 'intermediate value theorem', 'product', 'quotient', 'average', 'instantaneous', 'secant', 'change', 'infinite', 'infinity', 'proof','sum', 'cos(', 'sin(', 'tan(', 'ln(', 'function', `L'`, 'extrema', 'test', 'concave','convex','concavity','inflection','second','first','polar','parametric','relative','absolute','local','global','maximum', 'minimum', 'exponent', 'logarithm', 'tangent','sine','cosine','chain rule','related rate', 'volume', 'summation'];
 var chemWords = ['chemistry','chemical','gas','mole','state','solid','liquid','solution','reaction','pressure','volume','temperature','energy','work','enthalpy','entropy','endothermic','exothermic','molecule','solution','compound','ion','acid','atom','covalent','charge','polymer','metal','halogen','periodic table','transition','oxidation','reduction','cloud','configuration','valence','electron','proton','neutron','heat','specific','joule','group','nucleus','orbital','subshell','ionization','lewis','structure','octet','pair','phase','decomposition','displacement','single','double','precipitate','molality','molarity','density','base','conjugate','weak','strong','reactant','product','conservation','matter','redox','capacity','calorimeter']
 var historyWords = ['american', 'americans', 'army', 'british', 'colonies', 'congress', 'government', 'nation', 'quaker','native',  'north', 'party', 'president', 'rights', 'slavery', 'slaves', 'south', 'states', 'union', 'united', 'war', 'women', 'americas', 'culture', 'indian', 'indians', 'iroquois', 'tribe', 'america', 'became', 'britain', 'colonial', 'colonists', 'colony', 'england', 'english', 'florida', 'land', 'population', 'settlers', 'spain', 'spanish', 'bay', 'canada', 'colonization', 'columbia', 'empire', 'established', 'hudson', 'kingdom', 'overseas', 'quebec', 'rupert', 'boston', 'loyalists', 'parliament', 'patriots', 'revolution', 'york', 'french', 'hamilton', 'mount', 'vernon', 'virginia', 'washington', 'black', 'cotton', 'enslaved', 'free', 'slave', 'trade', 'white', 'battle', 'force', 'fort', 'navy', 'royal', 'ship', 'ships', 'era', 'federalist', 'federalists', 'feelings', 'good', 'jacksonian', 'madison', 'monroe', 'national', 'political', 'presidential', 'republican', 'tour', 'would', 'confederacy', 'confederate', 'southern', 'african', 'blacks',  'civil', 'former', 'freedmen', 'johnson', 'radical', 'radicals', 'reconstruction', 'republicans', 'state', 'vote', 'whites', 'abortion', 'carter', 'conservative', 'crisis', 'economic', 'ford', 'military', 'nixon', 'policy', 'reagan', 'soviet', 'vietnam', 'activists', 'alabama', 'bus', 'freedom', 'kennedy', 'king', 'local', 'malcolm', 'march', 'mississippi', 'montgomery', 'movement', 'nonviolence', 'nonviolent', 'police', 'public', 'registration', 'school', 'segregation',  'students', 'voter', 'voting', 'pennsylvania', 'new york', 'carolina', 'tobacco', 'plantation', 'master', 'liberty'];
-// function getWordsFromFile(fileToLoad) {
-  
-//   var xhr = new XMLHttpRequest();
-//   // document.getElementById('console').innerHTML = text2;
 
-//   xhr.open("GET", chrome.extension.getURL(fileToLoad), true);
-//   // document.getElementById('console').innerHTML = "2";
-
-
-//   xhr.addEventListener("load", function() {
-
-//     var result = xhr.responseText;
-//    // "result" should be the textbook entry for the subject
-//     // document.querySelector('#textBook').innerHTML = result;
-
-
-//     textBookText = result;
-   
-//     chrome.storage.local.set({txtbook: textBookText}, null);
-
-
-
-
-//   });
-
-//   xhr.send();
-// }
 
 function setNewSubject(){
   //Getting subject from chrome storage
@@ -154,85 +124,12 @@ chrome.runtime.onMessage.addListener(
   function(req, sender, sendResponse) {
     if (req.subject == "change subject") {
       setNewSubject();
-      // chrome.runtime.sendMessage(chrome.runtime.id, {redo: true}, null)
-    // } else {
-    //   function doSim() {
-    //     if(!subjectWords) {
-    //       setTimeout(doSim, 1000);
-    //       return;
-    //     }
-    //     // console.log('ploopsim');
-    //     var num = 0;
-    //     for(var i=0; i < subjectWords.length; i++){
-    //       if(req.text && req.text.includes(subjectWords[i]) === true){
-    //         num += 1;
-    //       }
-    //     }
-    //     var sim = num/(subjectWords.length);
-    //     console.log(num);
-    //     var text = req.txt;
-        
-    //     console.log(subjectWords)
-    //     console.log(text)
-    //     // console.log(req.txt);
-    //     chrome.storage.sync.get(['customerid', 'subject'], function(result){
-    //       // console.log(result);
-    //       // console.log(req.site)
-    //       var xhr = new XMLHttpRequest();
-    //       xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result.customerid}&site=${encodeURIComponent(req.site)}&sim=${sim}&subject=${result.subject}&loadsimtime=${req.loadsimtime}`);
-    //       xhr.send(); 
-    //     })
-    //     // console.log(sender.tab.url.split('.').slice(-1)[0]);
-    //     if (newSubject == "collegeApps"){
-    //       if (sender.tab.url.includes(".edu/") || sim > simCutoff || sender.tab.url.includes("college") || sender.tab.url.includes("university")){
-    //         sendResponse({res: false, sim: sim})
-    //       } else {
-    //         sendResponse({res: true, sim: sim, txt: "This ain't a college website"})
-    //       }
-    //     } else if(newSubject == "none"){
-    //         sendResponse({res: "power off", sim: sim})
-    //     } else if(newSubject == "hardBlock"){
-    //         sendResponse({res: true, sim: sim})
-    //     } else {
-    //       if (sim > simCutoff || sender.tab.url.includes(newSubject)) {
-    //         sendResponse({res: false, sim: sim})
-    //       } else {
-    //         sendResponse({res: true, sim: sim})
-    //       }
-    //     }
-        
-    //   }
-    //   doSim();
-    // }
+     
   }
 }
 )
 
-// chrome.runtime.onMessage.addListener(
-//   function(req, sender, sendResponse) {
-//     if (req.txt) {
-//       // console.log(sender.tab ?
-//       //   "from a content script:" + sender.tab.url:
-//       //   "from the extension");
-//       function doSim() {
-//         if(!textBookText) {
-//           setTimeout(doSim, 1000);
-//           return;
-//         }
-//         var sim = getSim(req.txt, textBookText)
-//         console.log(sim);
 
-//         if (sim < 2) {
-//           sendResponse({res: true, sim: sim, txt: textBookText})
-//         } else {
-//           sendResponse({res: false, sim: sim})
-//         }
-//       }
-//       doSim();
-
-//     }
-
-//   });
 
 chrome.runtime.onMessage.addListener(
   function(req, sender, sendResponse) {
@@ -258,118 +155,7 @@ chrome.runtime.onMessage.addListener(
 
   
 
-//Cos Sim begins
-// function genFreq(string) {
-//   string = string.toLowerCase();
-//   string = string.replace(/[^\w\s]|_/g, "");
-//   // string = string.replace("\n", " ");
-//   // var sentences = string.split('.');
-//   // for (i=0; i < sentences.length; i++){
-//   //   var wordCount = sentences[i].split(" ").map(w => w.length > 1);
-//   //   var cleanSentences = [];
-//   //   if(wordCount > 4){
-//   //     cleanSentences.push(sentences[i]);
-//   //   }
-//   // }
 
-//   // var betterString = cleanSentences.join(" ");
-  
-//   var wordArray = string.split(" ");
-//   var termFreqDict = {};
-//   for(i=0; i < wordArray.length; i++) {
-//     // wordArray[i] = stemmer(wordArray[i]);
-//     var w = wordArray[i];
-//     if(w !== w.replace(/[^\w\s]|_/g, ""))
-//       continue;
-//     if(wordArray[i] in termFreqDict){
-//       termFreqDict[wordArray[i]] = termFreqDict[wordArray[i]] + 1;
-
-//     } else {
-//       termFreqDict[wordArray[i]] = 1;
-//     }
-//   }
-//   return termFreqDict;
-// }
-
-// function union_arrays (x, y) {
-//   var obj = {};
-//   for (var i = x.length-1; i >= 0; -- i)
-//     obj[x[i]] = x[i];
-//   for (var i = y.length-1; i >= 0; -- i)
-//     obj[y[i]] = y[i];
-//   var res = []
-//   for (var k in obj) {
-//     if (obj.hasOwnProperty(k))  // <-- optional
-//       res.push(obj[k]);
-//   }
-//   return res;
-// }
-
-// function getSim(str1, str2){
-//   str1 = str1.split(" ").filter(w => !stopWords.includes(w)).join(" ");
-//   str2 = str2.split(" ").filter(w => !stopWords.includes(w)).join(" ");
-
-//   var dict1 = genFreq(str1)
-//   var dict2 = genFreq(str2)
-
-//   var str1Words = str1.toLowerCase().replace(/[^\w\s]|_/g, "").split(" ");
-//   var str2Words = str2.toLowerCase().replace(/[^\w\s]|_/g, "").split(" ");
-//   // str1Words = str1Words.map(word => stemmer(word));
-//   // str2Words = str2Words.map(word => stemmer(word));
-
-
-//   var allWords = union_arrays(str1Words, str2Words);
-
-//   var allDict = {};
-
-
-//   allWords.forEach(word => {
-//     var a = dict1[word] || 0;
-//     var b = dict2[word] || 0;
-//     allDict[word] = [a,b]
-//   })
-
-
-//   var dict1Norm = 0;
-//   var dict2Norm = 0;
-//   var sim = 0;
-//   debugger;
-//     for(key in allDict){
-//         dict1Norm += (allDict[key][0])**2
-//         dict2Norm += (allDict[key][1])**2
-//       }
-//     for(key in allDict){
-//         allDict[key][0] = (allDict[key][0])/Math.sqrt(dict1Norm)
-//         allDict[key][1] = (allDict[key][1])/Math.sqrt(dict2Norm)
-//       }
-//     for(key in allDict){
-//         sim += (allDict[key][0]*allDict[key][1])
-//       }
-//     // console.log('SIM SIM', sim)
-//     return sim
-// } 
-
-
-// chrome.runtime.onMessage.addListener(
-//   function(req, sender, sendResponse) {
-//     if (req.time) {
-//       // console.log('BOINK')
-//     chrome.storage.sync.get(['customerid'], function(result){
-//       console.log(result)
-//         var xhr = new XMLHttpRequest();
-//         xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result.customerid}&site=${encodeURIComponent(req.site)}&time=${req.time}`);
-//         xhr.send();
-//     })
-//   }
-//     else if (req.site) {
-//     chrome.storage.sync.get(['customerid'], function(result){
-//       // console.log(result)
-//         var xhr = new XMLHttpRequest();
-//         xhr.open("GET", `http://ontrackserver.herokuapp.com?id=${result.customerid}&site=${encodeURIComponent(req.site)}`);
-//       xhr.send();
-//     })
-//     }
-// })
 
 //Timer Code
 var timerInterval;
